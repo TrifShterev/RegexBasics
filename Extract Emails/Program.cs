23b505a140fe0c27c24233b0d88cb2f135553d94
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2020-07-21 15:19:11 +0300

    Add files via upload

---
+++ Extract Emails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s|^)(?<user>[A-Za-z0-9]([-._]?[A-Za-z0-9])*)@(?<host>[A-Za-z](\-?[A-Za-z])*(\.[A-Za-z](\-?[A-Za-z])*)+)";
+            string text = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(text,pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+                //Console.WriteLine($"--User: {match.Groups["user"].Value}");
+                //Console.WriteLine($"--Host: {match.Groups["host"].Value}");
+            }
+        }
+    }
+}
