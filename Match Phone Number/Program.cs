d31274e97bccf41a62e82d89a5f0e62ca8894aa1
Trifon Shterev <67158514+TrifShterev@users.noreply.github.com>
2020-07-21 15:23:23 +0300

    Add files via upload

---
+++ Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\+([359]+)([-| ])2(\2)(\d{3})(\2)(\d{4})\b");
+            MatchCollection patternMatch = regex.Matches(input);
+
+            var matchedPhones = patternMatch.Cast<Match>().Select(x => x.Value.Trim()).ToArray();
+            Console.WriteLine(String.Join(", ",matchedPhones));
+        }
+    }
+}
